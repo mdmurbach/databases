4f3b218d0fefabad6c6d201805ffedfe96252f26
mdmurbach <mmurbach@uw.edu>
2015-02-22 00:54:11 -0800

    Added code to connect to a database and insert values into predefined table

--- analyze.py
+++ analyze.py
@@ -1,4 +1,22 @@
 import psycopg2
 
-try:
-	conn = 
+
+conn_string = "dbname='BookData' user='mdmurbach' host='localhost' password='1423Ballard'"
+
+print "Connecting to database   -> %s" % (conn_string)
+
+conn = psycopg2.connect(conn_string)
+
+cursor = conn.cursor()
+print "Connected"
+
+sqlCode = "INSERT INTO Words(top_word,second_word,bookID) VALUES ('the','a',1);"
+
+cursor.execute(sqlCode)
+
+cursor.execute("SELECT * FROM Words")
+
+records = cursor.fetchall()
+
+print  records
+print "Done"
